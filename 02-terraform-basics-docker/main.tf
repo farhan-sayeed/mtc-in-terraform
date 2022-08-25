2f69ec1b1cad6f5779c7e177c47b49d12284b415
Ubuntu <ubuntu@ip-172-31-64-140.ec2.internal>
2022-08-25 11:40:52 +0000

    Imported using terraform

--- 02-terraform-basics-docker/main.tf
+++ 02-terraform-basics-docker/main.tf
@@ -14,7 +14,7 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "random_string" "random" {
-    count = 2
+    count = 1
     length = 4
     special = false
     upper = false
@@ -27,7 +27,7 @@ resource "random_string" "random" {
 # }
 
 resource "docker_container" "nodered_container" {
-    count = 2
+    count = 1
     name = join("-",["nodered", random_string.random[count.index].result])
     image = docker_image.nodered_image.latest
     ports {
@@ -36,7 +36,13 @@ resource "docker_container" "nodered_container" {
     }
 }
 
+resource "docker_container" "nodered_container2" {
+    name = "nodered-tgsl"
+    image = "docker_image.nodered_image.latest"
+}
 
+# For importing terraform container
+# terraform import docker_container.nodered_container $(docker inspect --format="{{.ID}}" nodered-3kki)
 # resource "docker_container" "nodered_container_2" {
 #     name = join("-",["nodered", random_string.random_2.result])
 #     image = docker_image.nodered_image.latest
@@ -50,22 +56,32 @@ resource "docker_container" "nodered_container" {
 #     description = "IP address of the container "
 # }
 
-output "container-name-1" {
-    value = docker_container.nodered_container[0].name
+# output "container-name-1" {
+#     value = docker_container.nodered_container[0].name
+#     description = "Name of the container"
+# }
+
+output "container-name" {
+    value = docker_container.nodered_container[*].name
     description = "Name of the container"
 }
 
-output "container-ip_address-1" {
-    value = join (":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
-    description = "IP address & external port of the container"
-}
+# output "container-ip_address-1" {
+#     value = join (":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+#     description = "IP address & external port of the container"
+# }
 
-output "container-name-2" {
-    value = docker_container.nodered_container[1].name
-    description = "Name of the container"
-}
+# output "container-name-2" {
+#     value = docker_container.nodered_container[1].name
+#     description = "Name of the container"
+# }
 
-output "container-ip_address-2" {
-    value = join (":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+# output "container-ip_address-2" {
+#     value = join (":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+#     description = "IP address & external port of the container"
+# }
+#IP address & port using for loop
+output "container-ip-address" {
+    value = [for i in docker_container.nodered_container[*]:join(":",[i.ip_address],i.ports[*]["external"])]
     description = "IP address & external port of the container"
 }
